f7607b01640fa84ee9da1deb2d84d181f39570b9
oy <Tom_Adams@web.de>
2010-08-16 04:09:21 +0200

    fixed that Collision::IntersectLine gets stuck in a loop forever. Closes #6

--- src/game/collision.cpp
+++ src/game/collision.cpp
@@ -183,11 +183,12 @@ void CCollision::GetSpeedup(int Index, vec2 *Dir, int *Force)
 int CCollision::IntersectLine(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2 *pOutBeforeCollision)
 {
 	float d = distance(Pos0, Pos1);
+	int End(d+1);
 	vec2 Last = Pos0;
 	
-	for(float f = 0; f < d; f++)
+	for(int i = 0; i < End; i++)
 	{
-		float a = f/d;
+		float a = i/d;
 		vec2 Pos = mix(Pos0, Pos1, a);
 		if(CheckPoint(Pos.x, Pos.y))
 		{
