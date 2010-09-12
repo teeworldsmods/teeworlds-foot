4fcf3b0230a0a3936e0c310c9c1400731e251518
oy <Tom_Adams@web.de>
2010-09-12 12:43:03 +0200

    close the emote selector, chat history and the scoreboard when closing the chat, console or ingame menu. Closes #111

--- src/game/client/components/scoreboard.cpp
+++ src/game/client/components/scoreboard.cpp
@@ -28,6 +28,11 @@ void CScoreboard::OnReset()
 	m_Active = false;
 }
 
+void CScoreboard::OnRelease()
+{
+	m_Active = false;
+}
+
 void CScoreboard::OnConsoleInit()
 {
 	Console()->Register("+scoreboard", "", CFGFLAG_CLIENT, ConKeyScoreboard, this, "Show scoreboard");
