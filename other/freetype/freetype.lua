c0a3413179a355c014365bdf2d573beefff0187e
SushiTee <masterkiller@Sascha-Weichels-iMac.local>
2010-11-28 19:45:56 +0100

    added freetype framework for mac

--- other/freetype/freetype.lua
+++ other/freetype/freetype.lua
@@ -6,16 +6,24 @@ FreeType = {
 			option.value = false
 			option.use_ftconfig = false
 			option.use_win32lib = false
+			option.use_osxframework = false
 			option.lib_path = nil
-			
+
 			if ExecuteSilent("freetype-config") > 0 and ExecuteSilent("freetype-config --cflags") == 0 then
 				option.value = true
 				option.use_ftconfig = true
 			end
-				
+			
 			if platform == "win32" then
 				option.value = true
 				option.use_win32lib = true
+			end
+
+			if platform == "macosx" then
+				print("test")
+				option.value = true
+				option.use_osxframework = true
+				option.use_ftconfig = false
 			end
 		end
 		
@@ -23,10 +31,13 @@ FreeType = {
 			-- include path
 			settings.cc.includes:Add(FreeType.basepath .. "/include")
 			
-			if option.use_ftconfig == true then
+			if option.use_osxframework == true then
+				settings.link.flags:Add("-Fother/freetype/mac")
+				client_settings.link.frameworks:Add("FreeType")
+				client_settings.cc.includes:Add("other/freetype/mac/FreeType.framework/Headers")
+			elseif option.use_ftconfig == true then
 				settings.cc.flags:Add("`freetype-config --cflags`")
 				settings.link.flags:Add("`freetype-config --libs`")
-				
 			elseif option.use_win32lib == true then
 				if settings.debug > 0 then
 					settings.link.libs:Add(FreeType.basepath .. "/lib/freetype243MT_D")
@@ -40,12 +51,14 @@ FreeType = {
 			output:option(option, "value")
 			output:option(option, "use_ftconfig")
 			output:option(option, "use_win32lib")
+			output:option(option, "use_osxframework")
 		end
 		
 		local display = function(option)
 			if option.value == true then
 				if option.use_ftconfig == true then return "using freetype-config" end
 				if option.use_win32lib == true then return "using supplied win32 libraries" end
+				if option.use_osxframework == true then return "using osx framework" end
 				return "using unknown method"
 			else
 				if option.required then
