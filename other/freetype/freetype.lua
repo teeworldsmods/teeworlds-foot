52242eb2a30895d0281803d0f4a78f517d9a1f88
SushiTee <weichel.sascha@xxxx.xx>
2010-11-29 16:10:17 +0100

    droping mac 10.4 compatibility and fixed compatibility to 10.5 when compiling in 10.6

--- other/freetype/freetype.lua
+++ other/freetype/freetype.lua
@@ -6,23 +6,16 @@ FreeType = {
 			option.value = false
 			option.use_ftconfig = false
 			option.use_win32lib = false
-			option.use_staticft = false
 			option.lib_path = nil
-
+			
 			if ExecuteSilent("freetype-config") > 0 and ExecuteSilent("freetype-config --cflags") == 0 then
 				option.value = true
 				option.use_ftconfig = true
 			end
-			
+				
 			if platform == "win32" then
 				option.value = true
 				option.use_win32lib = true
-			end
-
-			if platform == "macosx" then
-				option.value = true
-				option.use_ftconfig = false
-				option.use_staticft = true
 			end
 		end
 		
@@ -30,13 +23,10 @@ FreeType = {
 			-- include path
 			settings.cc.includes:Add(FreeType.basepath .. "/include")
 			
-			if option.use_staticft == true then
-				settings.cc.includes:Add("other/freetype/include")
-				client_settings.link.libpath:Add("other/freetype/lib")
-				client_settings.link.libs:Add("freetype243")
-			elseif option.use_ftconfig == true then
+			if option.use_ftconfig == true then
 				settings.cc.flags:Add("`freetype-config --cflags`")
 				settings.link.flags:Add("`freetype-config --libs`")
+				
 			elseif option.use_win32lib == true then
 				if settings.debug > 0 then
 					settings.link.libs:Add(FreeType.basepath .. "/lib/freetype243MT_D")
@@ -50,14 +40,12 @@ FreeType = {
 			output:option(option, "value")
 			output:option(option, "use_ftconfig")
 			output:option(option, "use_win32lib")
-			output:option(option, "use_staticft")
 		end
 		
 		local display = function(option)
 			if option.value == true then
 				if option.use_ftconfig == true then return "using freetype-config" end
 				if option.use_win32lib == true then return "using supplied win32 libraries" end
-				if option.use_staticft == true then return "using static freetype library" end
 				return "using unknown method"
 			else
 				if option.required then
