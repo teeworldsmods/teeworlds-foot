acb26d0717c2b6ba07401dbb1ae6c5645b41942c
oy <Tom_Adams@web.de>
2010-12-08 00:02:24 +0100

    added single function to stop recording by Batchyx

--- src/engine/client/client.h
+++ src/engine/client/client.h
@@ -291,6 +291,7 @@ public:
 
 	const char *DemoPlayer_Play(const char *pFilename, int StorageType);
 	void DemoRecorder_Start(const char *pFilename);
+	void DemoRecorder_Stop();
 
 	virtual class CEngine *Engine() { return &m_Engine; }
 };
