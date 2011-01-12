6c182e2ee7eaa4af0c9a09b341ec34d28fd244bd
oy <Tom_Adams@web.de>
2011-01-12 01:33:16 +0100

    close the scoreboard when opening the menu in the demo player. Closes #407

--- src/game/client/components/menus.cpp
+++ src/game/client/components/menus.cpp
@@ -1114,6 +1114,10 @@ void CMenus::SetActive(bool Active)
 			m_pClient->OnRelease();
 		}
 	}
+	else if(Client()->State() == IClient::STATE_DEMOPLAYBACK)
+	{
+		m_pClient->OnRelease();
+	}
 }
 
 void CMenus::OnReset()
