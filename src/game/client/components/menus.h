ce97d282d9546dd8446790d5b6675b2e198ee457
oy <Tom_Adams@web.de>
2011-03-13 10:41:10 +0100

    added extended demo infos in the demo browser

--- src/game/client/components/menus.h
+++ src/game/client/components/menus.h
@@ -6,6 +6,8 @@
 #include <base/vmath.h>
 #include <base/tl/sorted_array.h>
 
+#include <engine/demo.h>
+
 #include <game/client/component.h>
 #include <game/client/ui.h>
 
@@ -178,7 +180,7 @@ class CMenus : public CComponent
 		
 		bool m_InfosLoaded;
 		bool m_Valid;
-		char m_aMap[64];
+		CDemoHeader m_Info;
 		
 		bool operator<(const CDemoItem &Other) { return !str_comp(m_aFilename, "..") ? true : !str_comp(Other.m_aFilename, "..") ? false :
 														m_IsDir && !Other.m_IsDir ? true : !m_IsDir && Other.m_IsDir ? false :
