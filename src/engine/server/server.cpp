2ba3931536e4e823c33f3b55b313559c49ccc914
SushiTee <weichel.sascha@xxx.xx>
2011-05-21 19:15:53 +0200

    fixed compiling error

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -2,6 +2,7 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 
 #include <base/system.h>
+#include <base/math.h>
 
 #include <engine/config.h>
 #include <engine/console.h>
